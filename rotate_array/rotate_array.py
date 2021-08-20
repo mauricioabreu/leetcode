d142e1b2e4cad3c047b0a3e985b428c0f20a86f2
Maurício Antunes <mauricio.abreua@gmail.com>
2021-08-20 14:07:30 -0300

    Improve var name

--- rotate_array/rotate_array.py
+++ rotate_array/rotate_array.py
@@ -1,19 +1,19 @@
 def rotate(nums, k):
     size = len(nums)
     start_idx = 0
-    k = k % size
-    idx_to_update = start_idx + k
+    step = k % size
+    idx_to_update = start_idx + step
     num_to_update = nums[start_idx]
     i = 0
 
     while i < size:
         if idx_to_update != start_idx:
             num_to_update, nums[idx_to_update] = nums[idx_to_update], num_to_update
-            idx_to_update = (idx_to_update + k) % size
+            idx_to_update = (idx_to_update + step) % size
         else:
             nums[idx_to_update] = num_to_update
             start_idx = (start_idx + 1) % size
-            idx_to_update = (start_idx + k) % size
+            idx_to_update = (start_idx + step) % size
             num_to_update = nums[start_idx]
         i += 1
 
